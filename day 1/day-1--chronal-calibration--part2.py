a3f8b377d818d1a954cdfe785a60afe5d6cd88c6
alstroemerium <eternityzero@gmail.com>
2018-12-03 18:27:28 -0800

    Day 1 solution final

---
+++ day 1/day-1--chronal-calibration--part2.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+current_frequency = 0
+resulting_frequency = 0
+frequency_history = []
+end_condition = False
+# run_count = 1
+
+while not end_condition:
+    f = open("day-1--input", "r")
+    for input in f:
+        frequency_history.append(current_frequency)
+        frequency_delta = int(input)
+        resulting_frequency = current_frequency + frequency_delta
+        if resulting_frequency in frequency_history:
+            print resulting_frequency
+            end_condition = True
+            break
+        else:
+            current_frequency = resulting_frequency
+#    print "Run count: " + str(run_count)
+#    run_count += 1
